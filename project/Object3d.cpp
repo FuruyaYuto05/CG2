5134d5ad8f64030c8aeb6ab20bfb280cee6ee80e
FuruyaYuto <k024g0025@g.neec.ac.jp>
2025-12-08 16:57:42 +0900

    6-3P14

--- project/Object3d.cpp
+++ project/Object3d.cpp
@@ -1,5 +1,35 @@
 #include "Object3d.h"
+#include "Object3dCommon.h"
 
-void Object3d::Initialize()
+// 引数で受け取ってメンバ変数に記録する
+void Object3d::Initialize(Object3dCommon* object3dCommon)
 {
+    this->object3dCommon_ = object3dCommon;
+
+    // モデル読み込み
+    modelData_ = LoadObjFile("resources", "plane.obj");
+
+    // ----------------------------------------
+    // 頂点データの初期化（スライド内容）
+    // ----------------------------------------
+
+    // 頂点数
+    uint32_t vertexCount = (uint32_t)modelData_.vertices.size();
+    uint32_t bufferSize = sizeof(VertexData) * vertexCount;
+
+    // バッファリソースを作成
+    vertexBuffer_ = object3dCommon_->GetDxCommon()->CreateBufferResource(bufferSize);
+
+    // バッファにデータを書き込むためのアドレスを取得
+    vertexBuffer_->Map(0, nullptr, reinterpret_cast<void**>(&vertexData_));
+
+    // Objから読み込んだデータをコピー
+    for (uint32_t i = 0; i < vertexCount; i++) {
+        vertexData_[i] = modelData_.vertices[i];
+    }
+
+    // バッファビューの作成
+    vertexBufferView_.BufferLocation = vertexBuffer_->GetGPUVirtualAddress();
+    vertexBufferView_.SizeInBytes = bufferSize;
+    vertexBufferView_.StrideInBytes = sizeof(VertexData);
 }
--- project/Object3d.h
+++ project/Object3d.h
@@ -1,10 +1,51 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Math.h"
+
+// 前方宣言
+class Object3dCommon;
+
 class Object3d
 {
-
-public: /// メンバ関数
+public:
     // 初期化
-    void Initialize();
+    void Initialize(Object3dCommon* object3dCommon);
+
+private:
+    Object3dCommon* object3dCommon_ = nullptr;
+
+    struct VertexData {
+       Math::Vector4 position;
+       Math::Vector2 texcoord;
+       Math::Vector3 normal;
+    };
+
+    struct MaterialData {
+        std::string textureFilePath;
+    };
+
+    struct ModelData {
+        std::vector<VertexData> vertices;
+        MaterialData material;
+    };
+
+    // Objファイルのデータ
+    ModelData modelData_;
+
+    // mtlファイル読み取り
+    static MaterialData LoadMaterialTemplateFile(const std::string directoryPath, const std::string filename);
+
+    // objファイル読み取り
+    static ModelData LoadObjFile(const std::string directoryPath, const std::string filename);
+
+    // バッファリソース
+    Microsoft::WRL::ComPtr<ID3D12Resource> vertexBuffer_;
+
+    // バッファリソース内のデータを指すポインタ
+    VertexData* vertexData_ = nullptr;
+
+    // バッファビュー
+    D3D12_VERTEX_BUFFER_VIEW vertexBufferView_;
 
 };
-
